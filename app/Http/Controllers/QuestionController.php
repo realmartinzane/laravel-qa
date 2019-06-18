b3103e0ce1b984f475a70eb5f915a5b15f4021c3
Martin Zahariev <martinzahariev.official@gmail.com>
2019-06-18 08:48:48 +0200

    Implement show functionality for the questions

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -50,9 +50,10 @@ class QuestionController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function show(Question $q)
+    public function show(Question $question)
     {
-        //
+        $question->increment('views');
+        return view('questions.show', compact('question'));
     }
 
     /**
