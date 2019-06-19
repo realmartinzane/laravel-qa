d8bb203a976553f701a36e0ac557c61506f822ba
Martin Zahariev <martinzahariev.official@gmail.com>
2019-06-19 08:48:47 +0200

    Generate fake answers

--- database/factories/AnswerFactory.php
+++ database/factories/AnswerFactory.php
@@ -5,11 +5,11 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(App\Question::class, function (Faker $faker) {
+$factory->define(App\Answer::class, function (Faker $faker) {
     return [
-        'title' => rtrim($faker->sentence(rand(5, 10)), '.'),
         'body' => $faker->paragraphs(rand(3, 7), true),
-        'views' => rand(0, 10),
-        'votes' => rand(-3, 10)
+        'user_id' => App\User::pluck('id')->random(),
+        'votes_count' => rand(0, 5)
+
     ];
 });
